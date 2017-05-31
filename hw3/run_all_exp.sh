f551b01bde014ca2d6c28811e5bbd76cab4fbe45
xuhuazhe <xuhuazhe12@gmail.com>
2017-05-31 05:42:46 -0700

    dueling

--- hw3/run_all_exp.sh
+++ hw3/run_all_exp.sh
@@ -1,12 +1,24 @@
 #declare -a arr=("cross_entropy_plot()" "hinge_dqfd_plot()" "hard_Q_plot()" "soft_Q_plot()" "DQfD_no_l2_softQ_plot()" "DQfD_no_l2_plot()" "policy_gradient_soft_1_step_plot()" "exp_policy_grad_weighting_plot()" "exp_advantage_diff_learning_plot()")
-#declare -a arr=("collect_demonstration()")
+#declare -a arr=("policy_gradient_soft_1_step_finetune_small_explore_May16()")
 #declare -a arr=("DQFD_no_l2_T()" "DQfD_full_T()")
 #declare -a arr=("hard_Q_in_env()" "soft_Q_in_env()")
 #declare -a arr=("cross_entropy_dm_finetune_small_explore()" "cross_entropy_dm_finetune_normal_explore()")
 #declare -a arr=("cross_entropy_finetune()" "hinge_standard_finetune()" "hard_Q_finetune()")
-declare -a arr=("exp_advantage_diff_learning_visualize()" "exp_policy_gradient_visualize()")
-## now loop through the above array
+#declare -a arr=("policy_gradient_soft_1_step_finetune_small_explore_May16" "policy_gradient_soft_1_step_finetune_normal_explore_May16" "adv_learn_finetune_small_explore_May16" "adv_learn_finetune_normal_explore_May16" "exp_policy_grad_weighting_finetune_normal_explore_May16()" "exp_policy_grad_weighting_finetune_small_explore_May16()" "DQfD_no_l2_finetune_small_explore_human_May16()" "DQfD_no_l2_finetune_normal_explore_human_May16()")
+## now loop through the above arrayi
+declare -a arr=("dueling_net_double_Q()")
 for i in "${arr[@]}"
 do
-    python run_dqn_atari.py --config $i --dataset_size=300000 --bad_portion=0 &
+    python run_dqn_atari.py --config $i --bad_dir='/backup/hxu/modelRL/bad_demo_onpolicy.p' --bad_portion=0.25 &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --demo_file_path='/data/hxu/cs294-112/hw3/link_data/bad_demo_50000.0_0.3' &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --core_num=1 --demo_file_path='/data/hxu/cs294-112/hw3/link_data/bad_demo_150000.0_0.7' &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --core_num=2 --demo_file_path='/data/hxu/cs294-112/hw3/link_data/bad_demo_250000.0_0.7' &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --core_num=3 --demo_file_path='/data/hxu/cs294-112/hw3/link_data/bad_demo_50000.0_0.3' &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --core_num=0 --demo_file_path='/data/hxu/cs294-112/hw3/link_data/bad_demo_150000.0_0.3' &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --core_num=1 --demo_file_path='/data/hxu/cs294-112/hw3/link_data/bad_demo_250000.0_0.3' &
+	#python run_dqn_atari.py --config $i --dataset_size=300000 --bad_portion=0 --bad_starts=1.5e5 --tiny_explore=0.7 --core_num=1 &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --bad_portion=0 --bad_starts=0.5e5 --tiny_explore=0.7 --core_num=2 &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --bad_portion=0 --bad_starts=2.5e5 --tiny_explore=0.3 --core_num=3 &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --bad_portion=0 --bad_starts=1.5e5 --tiny_explore=0.3 --core_num=0 &
+    #python run_dqn_atari.py --config $i --dataset_size=300000 --bad_portion=0 --bad_starts=0.5e5 --tiny_explore=0.3 --core_num=1 &
 done
