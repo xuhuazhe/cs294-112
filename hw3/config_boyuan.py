c875d9f700712c346c72753cf45b06ce2e658997
Boyuan Chen <boyuanchen@berkeley.edu>
2019-08-17 14:33:30 -0700

    add more atari env script

--- hw3/config_boyuan.py
+++ hw3/config_boyuan.py
@@ -262,3 +262,113 @@ def dqn_doubledunk():
     FLAGS.collect_Q_experience = True
     FLAGS.learning_starts = 50000
     FLAGS.hard_Q_loss_weight = 1.0
+
+def dqn_alien():
+    tag = inspect.stack()[0][3]
+    stage = "from_scratch"
+    set_method_name(stage, tag)
+    set_unconditioned()
+    FLAGS.env_id = "AlienNoFrameskip-v4"
+    set_stage(stage)
+    set_eval_mode(False)
+    FLAGS.supervise_cross_entropy_loss_weight = 0.0
+    FLAGS.exp_value_critic_weighting = 0.0
+    FLAGS.exp_policy_grad_weighting = 0.0
+    FLAGS.tiny_explore = 0.01
+    FLAGS.batch_size = 32
+    FLAGS.method_name = "dqn_alien"
+    FLAGS.core_num = "0"
+    #Q learning specific
+    FLAGS.eval_freq = -1
+    FLAGS.demo_mode = "no_demo"
+    FLAGS.collect_Q_experience = True
+    FLAGS.learning_starts = 50000
+    FLAGS.hard_Q_loss_weight = 1.0
+
+def dqn_choppercommand():
+    tag = inspect.stack()[0][3]
+    stage = "from_scratch"
+    set_method_name(stage, tag)
+    set_unconditioned()
+    FLAGS.env_id = "ChopperCommandNoFrameskip-v4"
+    set_stage(stage)
+    set_eval_mode(False)
+    FLAGS.supervise_cross_entropy_loss_weight = 0.0
+    FLAGS.exp_value_critic_weighting = 0.0
+    FLAGS.exp_policy_grad_weighting = 0.0
+    FLAGS.tiny_explore = 0.01
+    FLAGS.batch_size = 32
+    FLAGS.method_name = "dqn_choppercommand"
+    FLAGS.core_num = "0"
+    #Q learning specific
+    FLAGS.eval_freq = -1
+    FLAGS.demo_mode = "no_demo"
+    FLAGS.collect_Q_experience = True
+    FLAGS.learning_starts = 50000
+    FLAGS.hard_Q_loss_weight = 1.0
+
+def dqn_solaris():
+    tag = inspect.stack()[0][3]
+    stage = "from_scratch"
+    set_method_name(stage, tag)
+    set_unconditioned()
+    FLAGS.env_id = "SolarisNoFrameskip-v4"
+    set_stage(stage)
+    set_eval_mode(False)
+    FLAGS.supervise_cross_entropy_loss_weight = 0.0
+    FLAGS.exp_value_critic_weighting = 0.0
+    FLAGS.exp_policy_grad_weighting = 0.0
+    FLAGS.tiny_explore = 0.01
+    FLAGS.batch_size = 32
+    FLAGS.method_name = "dqn_solaris"
+    FLAGS.core_num = "0"
+    #Q learning specific
+    FLAGS.eval_freq = -1
+    FLAGS.demo_mode = "no_demo"
+    FLAGS.collect_Q_experience = True
+    FLAGS.learning_starts = 50000
+    FLAGS.hard_Q_loss_weight = 1.0
+
+def dqn_privateeye():
+    tag = inspect.stack()[0][3]
+    stage = "from_scratch"
+    set_method_name(stage, tag)
+    set_unconditioned()
+    FLAGS.env_id = "PrivateEyeNoFrameskip-v4"
+    set_stage(stage)
+    set_eval_mode(False)
+    FLAGS.supervise_cross_entropy_loss_weight = 0.0
+    FLAGS.exp_value_critic_weighting = 0.0
+    FLAGS.exp_policy_grad_weighting = 0.0
+    FLAGS.tiny_explore = 0.01
+    FLAGS.batch_size = 32
+    FLAGS.method_name = "dqn_privateeye"
+    FLAGS.core_num = "0"
+    #Q learning specific
+    FLAGS.eval_freq = -1
+    FLAGS.demo_mode = "no_demo"
+    FLAGS.collect_Q_experience = True
+    FLAGS.learning_starts = 50000
+    FLAGS.hard_Q_loss_weight = 1.0
+
+def dqn_icehocky():
+    tag = inspect.stack()[0][3]
+    stage = "from_scratch"
+    set_method_name(stage, tag)
+    set_unconditioned()
+    FLAGS.env_id = "IceHockeyNoFrameskip-v4"
+    set_stage(stage)
+    set_eval_mode(False)
+    FLAGS.supervise_cross_entropy_loss_weight = 0.0
+    FLAGS.exp_value_critic_weighting = 0.0
+    FLAGS.exp_policy_grad_weighting = 0.0
+    FLAGS.tiny_explore = 0.01
+    FLAGS.batch_size = 32
+    FLAGS.method_name = "dqn_icehocky"
+    FLAGS.core_num = "0"
+    #Q learning specific
+    FLAGS.eval_freq = -1
+    FLAGS.demo_mode = "no_demo"
+    FLAGS.collect_Q_experience = True
+    FLAGS.learning_starts = 50000
+    FLAGS.hard_Q_loss_weight = 1.0
