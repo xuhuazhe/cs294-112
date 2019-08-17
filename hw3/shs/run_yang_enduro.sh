eae126b860304448fcbb6ee28f95ba9edbe43a66
Yang Gao <gy20073@gmail.com>
2019-08-17 14:02:07 -0700

    some install req in run_yang_enduro

--- hw3/shs/run_yang_enduro.sh
+++ hw3/shs/run_yang_enduro.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+# requirements
+# pip3 install gym[atari], tensorflow-gpu
+
 # this is intend to work with config_yang2.py
 cd ../
 
-declare -a arr=("enduro_perfectdemo_cross_entropy()" "enduro_perfectdemo_sal()")
+#declare -a arr=("enduro_perfectdemo_cross_entropy()" "enduro_perfectdemo_sal()")
+declare -a arr=("enduro_perfectdemo_sal()")
 
 for i in "${arr[@]}"
 do
-    python run_dqn_atari.py --config $i &
+    python3 run_dqn_atari.py --config $i &
     sleep 30
 done
