35e74596257f6b3b0a67fb5e7b26835bac5fcc2a
Boyuan Chen <boyuanchen@berkeley.edu>
2019-08-19 05:21:40 +0000

    sync from sylla

--- hw3/config_boyuan.py
+++ hw3/config_boyuan.py
@@ -394,3 +394,26 @@ def dqn_atlantis():
     FLAGS.collect_Q_experience = True
     FLAGS.learning_starts = 50000
     FLAGS.hard_Q_loss_weight = 1.0
+
+def dqn_enduro():
+    tag = inspect.stack()[0][3]
+    stage = "from_scratch"
+    set_method_name(stage, tag)
+    set_unconditioned()
+    FLAGS.env_id = "EnduroNoFrameskip-v4"
+    set_stage(stage)
+    set_eval_mode(False)
+    FLAGS.supervise_cross_entropy_loss_weight = 0.0
+    FLAGS.exp_value_critic_weighting = 0.0
+    FLAGS.exp_policy_grad_weighting = 0.0
+    FLAGS.tiny_explore = 0.01
+    FLAGS.batch_size = 32
+    FLAGS.method_name = "dqn_enduro"
+    FLAGS.core_num = "4"
+
+    #Q learning specific
+    FLAGS.eval_freq = -1
+    FLAGS.demo_mode = "no_demo"
+    FLAGS.collect_Q_experience = True
+    FLAGS.learning_starts = 50000
+    FLAGS.hard_Q_loss_weight = 1.0
