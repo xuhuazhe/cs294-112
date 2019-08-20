e7962c0ab1692e6ece1bffea3a6991a20a30df5f
Boyuan Chen <boyuanchen@berkeley.edu>
2019-08-19 20:42:17 -0700

    us epickle

--- hw3/Q_expert.py
+++ hw3/Q_expert.py
@@ -148,13 +148,11 @@ def collect(env,
             with open(log_file, 'a') as f:
                 print(t, mean_episode_reward, best_mean_episode_reward, file=f)
 
-
     # save the replay buffer
     print('save pickle! replay buffer has size: ', replay_buffer.num_in_buffer)
-    import hickle as hkl
-    Q_expert_path = '/home/boyuan/Projects/cs294-112/hw3/link_data/' + str(FLAGS.replay_buffer_size) + '_' + FLAGS.demo_name + '.hkl'
-    hkl.dump(replay_buffer, Q_expert_path, mode='w')
-
+    Q_expert_path = '/home/boyuan/Projects/cs294-112/hw3/link_data/' + str(FLAGS.replay_buffer_size) + '_' + FLAGS.demo_name + '.p'
+    with open(Q_expert_path, 'wb') as f:
+        p.dumps(replay_buffer, f, fix_imports=True)
 
 
 
